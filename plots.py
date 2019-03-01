fe68003fab6e4634afc18e296459f6ec6a9659a0
Viktor Toman <vtoman07@gmail.com>
2019-03-01 17:39:32 +0100

    Collect and plot for multiple algos

--- plots.py
+++ plots.py
@@ -7,102 +7,105 @@ import numpy as np
 import os
 
 
+# Algorithms
+ALGOS = ['sklearn', 'baseline', 'dtwithlc']
+
+
 def collect_stats(filename):
-  stats = {'names': [], 'sklearn': [], 'dtwithlc': [],
-           'max_sk': 1, 'max_lc': 1}
+  stats = {'names': []}
+  for algo in ALGOS:
+    stats[algo] = []
+    stats['%s_max' % algo] = 1
 
   namefollows = False
   disregard = True
-  c_name, c_sk, c_dtlc = '', -1, -1
+  c = {'name': ''}
+  for algo in ALGOS:
+    c[algo] = -1
   for line in open('results/reports/%s' % filename, 'r'):
     if '===' in line:
       if not disregard:
-        stats['names'].append(c_name)
-        stats['sklearn'].append(c_sk)
-        stats['dtwithlc'].append(c_dtlc)
-        if c_sk > stats['max_sk']:
-          stats['max_sk'] = c_sk
-        if c_dtlc > stats['max_lc']:
-          stats['max_lc'] = c_dtlc
-        if c_sk < c_dtlc:
-          print(c_name)
+        stats['names'].append(c['name'])
+        for algo in ALGOS:
+          stats[algo].append(c[algo])
+          if c[algo] > stats['%s_max' % algo]:
+            stats['%s_max' % algo] = c[algo]
       namefollows = True
       continue
     if namefollows:
       namefollows = False
-      c_name = line.strip()
-      c_sk, c_dtlc = -1, -1
+      c['name'] = line.strip()
+      for algo in ALGOS:
+        c[algo] = -1
       disregard = False
       continue
     if 'nodes' in line and 'correct' in line and 'time' in line:
-      if 'sklearn' in line:
-        assert(c_sk == -1)
-        l = line.split()
-        c_sk = int(l[1])
-        if l[3] != 'T':
-          print('SKLEARN TREE FOR %s IS NOT CORRECT!' % c_name)
-          disregard = True
-      elif 'dtwithlc' in line:
-        assert(c_dtlc == -1)
-        l = line.split()
-        c_dtlc = int(l[1])
-        if l[3] != 'T':
-          print('DTWITHLC TREE FOR %s IS NOT CORRECT!' % c_name)
-          disregard = True
+      for algo in ALGOS:
+        if algo in line:
+          assert(c[algo] == -1)
+          l = line.split()
+          c[algo] = int(l[1])
+          if l[3] != 'T':
+            print('%s TREE FOR %s IS NOT CORRECT!' % (algo, c['name']))
+            disregard = True
+          break
 
-  for stat in ['names', 'sklearn', 'dtwithlc']:
-    stats[stat] = np.array(stats[stat])
+  stats['names'] = np.array(stats['names'])
+  for algo in ALGOS:
+    stats[algo] = np.array(stats[algo])
   return stats
 
 
-def all_plot(stats, plotname):
-  SHIFT = 1.1
-  l = stats['sklearn'].size
-  m = max(stats['max_sk'], stats['max_lc'])
-  assert(l == stats['dtwithlc'].size)
+def all_plot(stats, x_algo, y_algo, plotname):
+  l = stats[x_algo].size
+  assert(l == stats[y_algo].size)
+  m = max(stats['%s_max' % x_algo], stats['%s_max' % y_algo])
   plt.figure(num=None, figsize=(15, 6), dpi=150, facecolor='w', edgecolor='k')
-  plt.scatter(range(l), stats['sklearn']+SHIFT,
-              color='b', edgecolor='black', linewidth=0.6, alpha=0.8)
-  plt.scatter(range(l), stats['dtwithlc']+SHIFT,
-              color='r', edgecolor='black', linewidth=0.6, alpha=0.8)
-  plt.ylim([1, m * 1.1])
+  ind = np.lexsort((stats[x_algo], stats[y_algo])) # first by y_algo
+  plt.scatter(range(l), [stats[x_algo][i] for i in ind],
+              color='b', edgecolor='black', linewidth=0.4, alpha=1.)
+  plt.scatter(range(l), [stats[y_algo][i] for i in ind],
+              color='r', edgecolor='black', linewidth=0.4, alpha=1.)
+  plt.ylim([0.9, m * 1.1])
   plt.xlim([-(l-1) * 0.007, (l-1) * 1.007])
   ax = plt.gca()
   ax.set_yscale('log', basey=np.e)
-  y_ticks = np.logspace(np.log(SHIFT), np.log(m+SHIFT), num=10, base=np.e)
+  y_ticks = np.logspace(np.log(1.), np.log(m), num=10, base=np.e)
   ax.set_yticks(y_ticks)
-  ax.set_yticklabels(np.ceil(y_ticks-SHIFT).astype(int))
+  ax.set_yticklabels(np.ceil(y_ticks).astype(int))
   plt.xlabel('Benchmark number', fontsize=18)
   plt.ylabel('Decision tree size', fontsize=18)
-  blue_patch = mpatches.Patch(color='blue', label='Scikit-learn')
-  red_patch = mpatches.Patch(color='red', label='DTwithLC')
+  blue_patch = mpatches.Patch(color='blue', label=x_algo)
+  red_patch = mpatches.Patch(color='red', label=y_algo)
   plt.legend(handles=[blue_patch, red_patch], loc=4)
   if not os.path.exists('results/plots'):
     os.makedirs('results/plots')
-  plt.savefig('results/plots/%s_all_plot.png' % plotname, bbox_inches='tight')
+  plt.savefig('results/plots/%s_all_plot_%s_%s.png' %
+              (plotname, x_algo, y_algo), bbox_inches='tight')
 
 
-def versus_plot(stats, plotname):
+def versus_plot(stats, x_algo, y_algo, plotname):
   plt.figure(num=None, figsize=(12, 6), dpi=150, facecolor='w', edgecolor='k')
-  plt.scatter(stats['sklearn'], stats['dtwithlc'],
+  plt.scatter(stats[x_algo], stats[y_algo],
               color='b', edgecolor='black', linewidth=0.6, alpha=0.8)
   plt.plot([0, 100000], [0, 100000], color='r', linewidth=2.0, alpha=0.4)
   plt.plot([0, 100000], [0, 10000], color='r', linewidth=2.0, alpha=0.2)
-  plt.xlim([-1, stats['max_sk'] * 1.01])
-  plt.ylim([-1, stats['max_lc'] * 1.015])
-  plt.xlabel('Scikit-learn size', fontsize=18)
-  plt.ylabel('DTwithLC size', fontsize=18)
+  plt.xlim([-1, stats['%s_max' % x_algo] * 1.01])
+  plt.ylim([-1, stats['%s_max' % y_algo] * 1.015])
+  plt.xlabel('%s size' % x_algo, fontsize=18)
+  plt.ylabel('%s size' % y_algo, fontsize=18)
   if not os.path.exists('results/plots'):
     os.makedirs('results/plots')
-  plt.savefig('results/plots/%s_vs_plot.png' % plotname, bbox_inches='tight')
+  plt.savefig('results/plots/%s_vs_plot_%s_%s.png' %
+              (plotname, x_algo, y_algo), bbox_inches='tight')
 
 
-def ratio_plot(stats, plotname):
-  l = stats['sklearn'].size
-  assert(l == stats['dtwithlc'].size)
+def ratio_plot(stats, x_algo, y_algo, plotname):
+  l = stats[x_algo].size
+  assert(l == stats[y_algo].size)
   plt.figure(num=None, figsize=(12, 6), dpi=150, facecolor='w', edgecolor='k')
   plt.clf()
-  plt.scatter(range(l), stats['dtwithlc'] / stats['sklearn'],
+  plt.scatter(range(l), stats[y_algo] / stats[x_algo],
               color='b', edgecolor='black', linewidth=0.6, alpha=0.8)
   plt.plot([-10, 100000], [1, 1], color='r', linewidth=4.0, alpha=0.6)
   plt.ylim([0.001, 3.])
@@ -113,10 +116,11 @@ def ratio_plot(stats, plotname):
   ax.set_yticks(ticks)
   ax.set_yticklabels(ticks)
   plt.xlabel('Benchmark number', fontsize=18)
-  plt.ylabel('DTwitLC/Scikit-learn size ratio', fontsize=18)
+  plt.ylabel('%s/%s size ratio' % (y_algo, x_algo), fontsize=18)
   if not os.path.exists('results/plots'):
     os.makedirs('results/plots')
-  plt.savefig('results/plots/%s_ratio_plot.png' % plotname, bbox_inches='tight')
+  plt.savefig('results/plots/%s_ratio_plot_%s_%s.png' %
+              (plotname, x_algo, y_algo), bbox_inches='tight')
 
 
 def main():
@@ -127,9 +131,9 @@ def main():
       print(filename)
       stats = collect_stats(filename)
       plotname = filename.replace('report_', '').replace('.txt', '')
-      all_plot(stats, plotname)
-      versus_plot(stats, plotname)
-      ratio_plot(stats, plotname)
+      all_plot(stats, 'baseline', 'dtwithlc', plotname)
+      versus_plot(stats, 'baseline', 'dtwithlc', plotname)
+      ratio_plot(stats, 'baseline', 'dtwithlc', plotname)
 
 
 main()
