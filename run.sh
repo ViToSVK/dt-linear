2c9efe2e10b02c0d20f8ea7271987f6ca573a842
Viktor Toman <vtoman07@gmail.com>
2019-03-01 14:46:48 +0100

    Experiments on permissive strategies

--- run.sh
+++ run.sh
@@ -3,3 +3,4 @@ python experiments.py mdps_consensus debug replace
 python experiments.py mdps_csma debug replace
 python experiments.py games_randomltl_encoded debug replace
 python experiments.py games_wash debug replace
+python experiments.py games_wash_permissive debug replace
