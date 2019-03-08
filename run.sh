7491ffda3c384594baa68ab2253021181cfa1d0d
Viktor Toman <vtoman07@gmail.com>
2019-03-08 11:29:30 +0100

    coBuchi experiments in script

--- run.sh
+++ run.sh
@@ -4,3 +4,4 @@ python experiments.py mdps_csma debug replace
 python experiments.py games_randomltl_encoded debug replace
 python experiments.py games_wash debug replace
 python experiments.py games_wash_permissive debug replace
+python experiments.py mdps_coBuchiAS_randomltl debug replace
